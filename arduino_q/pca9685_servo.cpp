d0551dba625e3797c15f55fff9be573e31b2fdda
10210804 <3366479349@qq.com>
2025-10-10 16:41:04 +0800

    Update pca9685_servo.cpp

--- arduino_q/pca9685_servo.cpp
+++ arduino_q/pca9685_servo.cpp
@@ -27,7 +27,7 @@ void controlSG90Relative(int channel, int relativeAngle) {
   tcaSelect(TCA_PCA9685_CH);
   uint16_t pulse = relativeAngleToPulseSG90(relativeAngle);
   pca.setPWM(channel, 0, pulse);
-  delay(500);
+  delay(1000);
 }
 
 void resetSG90(int channel) {
